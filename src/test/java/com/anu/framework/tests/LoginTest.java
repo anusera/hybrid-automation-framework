d59c6a42395edb3c1ccc1c377e64caf7a1176136
anusera <84999479+anusera@users.noreply.github.com>
2026-06-09 11:03:57 +0530

    Implemented log4j2 in LoginTest

--- src/test/java/com/anu/framework/tests/LoginTest.java
+++ src/test/java/com/anu/framework/tests/LoginTest.java
@@ -1,5 +1,7 @@
 package com.anu.framework.tests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,18 +11,26 @@ import com.anu.framework.pages.ProductsPage;
 
 public class LoginTest extends BaseTest {
 	
+	public static final Logger logger = LogManager.getLogger(LoginTest.class);
+	
 	@Test(dataProvider = "loginData", dataProviderClass = com.anu.framework.dataproviders.ValidLoginDataProvider.class)
 	public void testValidLogin(String username, String password) {
+		logger.info("Starting test: testValidLogin with username: " + username);
 		LoginPage loginPage = new LoginPage(driver);
+		logger.info("Attempting to login with username: " + username);
 		loginPage.login(username,password);
 		ProductsPage productsPage = new ProductsPage(driver);
-		Assert.assertEquals(productsPage.getProductTitle(), "Products", "Login failed or Products page not displayed");			
+		Assert.assertEquals(productsPage.getProductTitle(), "Products", "Login failed or Products page not displayed");	
+		logger.info("Login successful, Products page displayed with title: " + productsPage.getProductTitle());
 	}
 	
 	@Test(dataProvider = "invalidLoginData", dataProviderClass = com.anu.framework.dataproviders.InvalidLoginDataProvider.class)
 	public void testInvalidLogin(String username, String password) {
+		logger.info("Starting test: testInvalidLogin with username: " + username);
 		LoginPage loginPage = new LoginPage(driver);
+		logger.info("Attempting to login with invalid credentials: " + username);
 		loginPage.login(username,password);
 		Assert.assertTrue(loginPage.isErrorMessageDisplayed(), "Error message not displayed for invalid login");
+		logger.info("Invalid login attempt correctly displayed error message for username: " + username);
 	}
 }
